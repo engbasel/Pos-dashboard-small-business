08097bb217d4ddd9b449f9decea61ec6b0e22c8a
Basel Embaby <engbasel2025@gmail.com>
2024-07-08 23:53:21 +0300

    "Refactored main.dart to use POS_system widget and removed MyHomePage and _MyHomePageState classes; updated widget_test.dart to test POS_system widget"

---
+++ lib/Futuers/Views/main_dashboadrd_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:pos_dashboard_v1/Futuers/Views/DesktopLayout.dart';
+import 'package:pos_dashboard_v1/Futuers/Views/MobileLayout.dart';
+import 'package:pos_dashboard_v1/Futuers/Views/TabletLayout.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth >= 1200) {
+            // Desktop layout
+            return const DesktopLayout();
+          } else if (constraints.maxWidth >= 600) {
+            // Tablet layout
+            return const TabletLayout();
+          } else {
+            // Mobile layout
+            return const MobileLayout();
+          }
+        },
+      ),
+    );
+  }
+}

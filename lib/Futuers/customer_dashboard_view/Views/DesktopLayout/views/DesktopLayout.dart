e17e15bde00d3d58b4946847ea10d97eb18f016a
Basel Embaby <engbasel2025@gmail.com>
2024-07-09 16:18:06 +0300

    Add drawer item to desktop layout and refactor ui

--- lib/Futuers/customer_dashboard_view/Views/DesktopLayout/views/DesktopLayout.dart
+++ lib/Futuers/customer_dashboard_view/Views/DesktopLayout/views/DesktopLayout.dart
@@ -1,13 +1,216 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:pos_dashboard_v1/manager.dart';
 
-class DesktopLayout extends StatelessWidget {
+// class DesktopLayout extends StatefulWidget {
+//   const DesktopLayout({super.key});
+
+//   @override
+//   State<DesktopLayout> createState() => _DesktopLayoutState();
+// }
+
+// class _DesktopLayoutState extends State<DesktopLayout> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       drawer: const CustomDrawer(),
+//       body: const Row(
+//         children: [
+//           // Add other widgets for the main content of your desktop layout
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class CustomDrawer extends StatelessWidget {
+//   const CustomDrawer({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Drawer(
+//       backgroundColor: ColorsManager.drawerColor,
+//       child: ListView(
+//         padding: const EdgeInsets.only(top: 125),
+//         children: [
+//           DrawerItem(
+//             icon: Icons.dashboard,
+//             text: 'Overview',
+//             onTap: () {},
+//           ),
+//           DrawerItem(
+//             icon: Icons.people,
+//             text: 'Customers',
+//             isSelected: true,
+//             onTap: () {},
+//           ),
+//           DrawerItem(
+//             icon: Icons.settings,
+//             text: 'Settings',
+//             onTap: () {},
+//           ),
+//           DrawerItem(
+//             icon: Icons.message,
+//             text: 'Messages',
+//             onTap: () {},
+//             badgeCount: 5,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget DrawerItem({
+//     required IconData icon,
+//     required String text,
+//     GestureTapCallback? onTap,
+//     bool isSelected = false,
+//     int badgeCount = 0,
+//   }) {
+//     return ListTile(
+//       title: Row(
+//         children: <Widget>[
+//           Icon(
+//             icon,
+//             color: isSelected ? ColorsManager.selectedIconColor : null,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.only(left: 8.0),
+//             child: Text(
+//               text,
+//               style: TextStyle(
+//                 color: isSelected ? ColorsManager.TextColor : null,
+//               ),
+//             ),
+//           ),
+//           if (badgeCount > 0)
+//             Padding(
+//               padding: const EdgeInsets.only(left: 8.0),
+//               child: Container(
+//                 padding: const EdgeInsets.all(2),
+//                 decoration: BoxDecoration(
+//                   color: ColorsManager.badgeColor,
+//                   borderRadius: BorderRadius.circular(12),
+//                 ),
+//                 constraints: const BoxConstraints(
+//                   minWidth: 16,
+//                   minHeight: 16,
+//                 ),
+//                 child: Center(
+//                   child: Text(
+//                     '$badgeCount',
+//                     style: const TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 12,
+//                     ),
+//                     textAlign: TextAlign.center,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//         ],
+//       ),
+//       selected: isSelected,
+//       onTap: onTap,
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:pos_dashboard_v1/manager.dart';
+
+import '../Widgets/drwer_item.dart';
+
+class DesktopLayout extends StatefulWidget {
   const DesktopLayout({super.key});
 
   @override
+  State<DesktopLayout> createState() => _DesktopLayoutState();
+}
+
+class _DesktopLayoutState extends State<DesktopLayout> {
+  int selectedDrawerIndex = 0;
+
+  void onSelectDrawerItem(int index) {
+    setState(() {
+      selectedDrawerIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Desktop Layout'),
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: CustomDrawer(
+        selectedIndex: selectedDrawerIndex,
+        onSelectItem: onSelectDrawerItem,
+      ),
+      body: const Row(
+        children: [
+          // Add other widgets for the main content of your desktop layout
+        ],
+      ),
+    );
+  }
+}
+
+class CustomDrawer extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onSelectItem;
+
+  const CustomDrawer({
+    super.key,
+    required this.selectedIndex,
+    required this.onSelectItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: selectedIndex >= 0
+          ? ColorsManager.drawerColor
+          : ColorsManager.drawerColor,
+      child: ListView(
+        padding: const EdgeInsets.only(top: 125),
+        children: [
+          DrawerItem(
+            index: 0,
+            icon: Icons.dashboard,
+            text: 'Overview',
+            isSelected: selectedIndex == 0,
+            onTap: () {
+              onSelectItem(0);
+            },
+          ),
+          DrawerItem(
+            index: 1,
+            icon: Icons.people,
+            text: 'Customers',
+            isSelected: selectedIndex == 1,
+            onTap: () {
+              onSelectItem(1);
+            },
+          ),
+          DrawerItem(
+            index: 2,
+            icon: Icons.settings,
+            text: 'Settings',
+            isSelected: selectedIndex == 2,
+            onTap: () {
+              onSelectItem(2);
+            },
+          ),
+          DrawerItem(
+            index: 3,
+            icon: Icons.message,
+            text: 'Messages',
+            isSelected: selectedIndex == 3,
+            onTap: () {
+              onSelectItem(3);
+            },
+            badgeCount: 5,
+          ),
+        ],
       ),
     );
   }
---
+++ lib/manager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color drawerColor = Color(0xFF2E2E2E);
+  static const Color headerColor = Colors.green;
+  static const Color headerTextColor = Colors.white;
+  static const Color selectedIconColor = Colors.green;
+  static const Color selectedTextColor = Colors.green;
+  static const Color badgeColor = Colors.green;
+  static const Color TextColor = Color(0x000ca37a);
+}

94f6668e8c57860864f32bf131ac6c9ca71d978f
Basel Embaby <engbasel2025@gmail.com>
2024-07-10 20:41:10 +0300

    "Updated breakpoint for desktop layout in DashboardView and added screen size logging in main.dart"

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class pos_system extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    print(width);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(

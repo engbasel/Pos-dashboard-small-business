0c574c6d44c3b5a91dd2ac9e53bae1a844cdeb95
Basel Embaby <engbasel2025@gmail.com>
2024-07-10 20:41:35 +0300

    "Removed width and height variables and print statement from pos_system widget"

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,6 @@ class pos_system extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    print(width);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(

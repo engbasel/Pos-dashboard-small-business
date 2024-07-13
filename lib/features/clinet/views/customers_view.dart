826ade53af8f42d3369dabc915049e86c8392080
Basel Embaby <engbasel2025@gmail.com>
2024-07-13 14:24:29 +0300

    refactor delet and edit methods in  coustomer view

--- lib/features/clinet/views/customers_view.dart
+++ lib/features/clinet/views/customers_view.dart
@@ -1,8 +1,145 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
+// import '../../../core/db/clients_database.dart';
+// import '../widgets/AddCustomerDialog.dart';
+// import '../widgets/CostomCoustomDitelsCard.dart';
+// import '../widgets/CustomerDetailView.dart';
+
+// class CustomersView extends StatefulWidget {
+//   const CustomersView({super.key});
+
+//   @override
+//   _CustomersViewState createState() => _CustomersViewState();
+// }
+
+// class _CustomersViewState extends State<CustomersView> {
+//   final Customers_helper dbHelper = Customers_helper();
+//   List<Map<String, dynamic>> customers = [];
+//   List<Map<String, dynamic>> filteredCustomers = [];
+//   final TextEditingController searchController = TextEditingController();
+//   String searchQuery = '';
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchCustomers();
+//     searchController.addListener(() {
+//       setState(() {
+//         searchQuery = searchController.text;
+//         filterCustomers();
+//       });
+//     });
+//   }
+
+//   Future<void> fetchCustomers() async {
+//     List<Map<String, dynamic>> fetchedCustomers = await dbHelper.getCustomers();
+//     setState(() {
+//       customers = fetchedCustomers;
+//       filteredCustomers = customers;
+//     });
+//   }
+
+//   void addCustomer(Map<String, String> customer) async {
+//     await dbHelper.insertCustomer(customer);
+//     fetchCustomers();
+//   }
+
+//   void updateCustomer(Map<String, String> customer) async {
+//     await dbHelper.updateCustomer(customer);
+//     fetchCustomers();
+//   }
+
+//   void deleteCustomer(int id) async {
+//     await dbHelper.deleteCustomer(id);
+//     fetchCustomers();
+//   }
+
+//   void filterCustomers() {
+//     setState(() {
+//       if (searchQuery.isEmpty) {
+//         filteredCustomers = customers;
+//       } else {
+//         filteredCustomers = customers
+//             .where((customer) => customer['fullName']
+//                 .toLowerCase()
+//                 .contains(searchQuery.toLowerCase()))
+//             .toList();
+//       }
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     searchController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: TextField(
+//               controller: searchController,
+//               decoration: InputDecoration(
+//                 labelText: 'Search',
+//                 suffixIcon: IconButton(
+//                   icon: const Icon(Icons.clear),
+//                   onPressed: () {
+//                     searchController.clear();
+//                   },
+//                 ),
+//               ),
+//             ),
+//           ),
+//           Expanded(
+//             child: filteredCustomers.isEmpty
+//                 ? const Center(child: Text('Client not found'))
+//                 : ListView(
+//                     children: filteredCustomers
+//                         .map((customer) => CoustomDitelsCard(
+//                               customer: customer,
+//                               onEdit: updateCustomer,
+//                               onDelete: deleteCustomer,
+//                               onTap: () {
+//                                 Navigator.of(context).push(MaterialPageRoute(
+//                                   builder: (context) => CustomerDetailView(
+//                                     customer: customer,
+//                                     onEdit: updateCustomer,
+//                                     onDelete: deleteCustomer,
+//                                   ),
+//                                 ));
+//                               },
+//                             ))
+//                         .toList(),
+//                   ),
+//           ),
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () async {
+//           final newCustomer = await showDialog<Map<String, String>>(
+//             context: context,
+//             builder: (context) => const AddCustomerDialog(),
+//           );
+//           if (newCustomer != null) {
+//             addCustomer(newCustomer);
+//           }
+//         },
+//         tooltip: 'Add Customer',
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:pos_dashboard_v1/features/clinet/widgets/CostomCoustomDitelsCard.dart';
 import '../../../core/db/clients_database.dart';
 import '../widgets/AddCustomerDialog.dart';
-import '../widgets/CostomCoustomDitelsCard.dart';
 import '../widgets/CustomerDetailView.dart';
 
 class CustomersView extends StatefulWidget {
@@ -35,7 +172,7 @@ class _CustomersViewState extends State<CustomersView> {
     List<Map<String, dynamic>> fetchedCustomers = await dbHelper.getCustomers();
     setState(() {
       customers = fetchedCustomers;
-      filteredCustomers = customers;
+      filterCustomers();
     });
   }
 
@@ -57,7 +194,7 @@ class _CustomersViewState extends State<CustomersView> {
   void filterCustomers() {
     setState(() {
       if (searchQuery.isEmpty) {
-        filteredCustomers = customers;
+        filteredCustomers = List.from(customers);
       } else {
         filteredCustomers = customers
             .where((customer) => customer['fullName']
@@ -98,23 +235,33 @@ class _CustomersViewState extends State<CustomersView> {
           Expanded(
             child: filteredCustomers.isEmpty
                 ? const Center(child: Text('Client not found'))
-                : ListView(
-                    children: filteredCustomers
-                        .map((customer) => CostomCoustomDitelsCard(
+                : ListView.builder(
+                    itemCount: filteredCustomers.length,
+                    itemBuilder: (context, index) {
+                      final customer = filteredCustomers[index];
+                      return CoustomDitelsCard(
+                        customer: customer,
+                        onEdit: (editedCustomer) {
+                          updateCustomer(editedCustomer);
+                        },
+                        onDelete: (id) {
+                          deleteCustomer(id);
+                        },
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => CustomerDetailView(
                               customer: customer,
-                              onEdit: updateCustomer,
-                              onDelete: deleteCustomer,
-                              onTap: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => CustomerDetailView(
-                                    customer: customer,
-                                    onEdit: updateCustomer,
-                                    onDelete: deleteCustomer,
-                                  ),
-                                ));
+                              onEdit: (editedCustomer) {
+                                updateCustomer(editedCustomer);
                               },
-                            ))
-                        .toList(),
+                              onDelete: (id) {
+                                deleteCustomer(id);
+                              },
+                            ),
+                          ));
+                        },
+                      );
+                    },
                   ),
           ),
         ],
--- lib/features/clinet/widgets/EditCustomerDialog.dart
+++ lib/features/clinet/widgets/EditCustomerDialog.dart
@@ -1,3 +1,82 @@
+// import 'package:flutter/material.dart';
+
+// class EditCustomerDialog extends StatefulWidget {
+//   final Map<String, dynamic> customer;
+
+//   const EditCustomerDialog({required this.customer, super.key});
+
+//   @override
+//   _EditCustomerDialogState createState() => _EditCustomerDialogState();
+// }
+
+// class _EditCustomerDialogState extends State<EditCustomerDialog> {
+//   final _formKey = GlobalKey<FormState>();
+//   late Map<String, String> _customerData;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _customerData = Map<String, String>.from(widget.customer);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AlertDialog(
+//       title: const Text('Edit Customer'),
+//       content: Form(
+//         key: _formKey,
+//         child: SingleChildScrollView(
+//           child: Column(
+//             children: [
+//               TextFormField(
+//                 initialValue: _customerData['fullName'],
+//                 decoration: const InputDecoration(labelText: 'Full Name'),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Please enter full name';
+//                   }
+//                   return null;
+//                 },
+//                 onSaved: (value) => _customerData['fullName'] = value!,
+//               ),
+//               TextFormField(
+//                 initialValue: _customerData['indebtedness'],
+//                 decoration: const InputDecoration(labelText: 'Indebtedness'),
+//                 onSaved: (value) => _customerData['indebtedness'] = value!,
+//               ),
+//               TextFormField(
+//                 initialValue: _customerData['currentAccount'],
+//                 decoration: const InputDecoration(labelText: 'Current Account'),
+//                 onSaved: (value) => _customerData['currentAccount'] = value!,
+//               ),
+//               TextFormField(
+//                 initialValue: _customerData['notes'],
+//                 decoration: const InputDecoration(labelText: 'Notes'),
+//                 onSaved: (value) => _customerData['notes'] = value!,
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//       actions: [
+//         TextButton(
+//           onPressed: () => Navigator.of(context).pop(),
+//           child: const Text('Cancel'),
+//         ),
+//         ElevatedButton(
+//           onPressed: () {
+//             if (_formKey.currentState!.validate()) {
+//               _formKey.currentState!.save();
+//               Navigator.of(context).pop(_customerData);
+//             }
+//           },
+//           child: const Text('Save'),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class EditCustomerDialog extends StatefulWidget {
@@ -11,12 +90,18 @@ class EditCustomerDialog extends StatefulWidget {
 
 class _EditCustomerDialogState extends State<EditCustomerDialog> {
   final _formKey = GlobalKey<FormState>();
-  late Map<String, String> _customerData;
+  late Map<String, String> _editedCustomer;
 
   @override
   void initState() {
     super.initState();
-    _customerData = Map<String, String>.from(widget.customer);
+    _editedCustomer = {
+      'id': widget.customer['id'].toString(),
+      'fullName': widget.customer['fullName'],
+      'indebtedness': widget.customer['indebtedness'],
+      'currentAccount': widget.customer['currentAccount'],
+      'notes': widget.customer['notes'],
+    };
   }
 
   @override
@@ -27,9 +112,10 @@ class _EditCustomerDialogState extends State<EditCustomerDialog> {
         key: _formKey,
         child: SingleChildScrollView(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               TextFormField(
-                initialValue: _customerData['fullName'],
+                initialValue: _editedCustomer['fullName'],
                 decoration: const InputDecoration(labelText: 'Full Name'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -37,22 +123,22 @@ class _EditCustomerDialogState extends State<EditCustomerDialog> {
                   }
                   return null;
                 },
-                onSaved: (value) => _customerData['fullName'] = value!,
+                onSaved: (value) => _editedCustomer['fullName'] = value!,
               ),
               TextFormField(
-                initialValue: _customerData['indebtedness'],
+                initialValue: _editedCustomer['indebtedness'],
                 decoration: const InputDecoration(labelText: 'Indebtedness'),
-                onSaved: (value) => _customerData['indebtedness'] = value!,
+                onSaved: (value) => _editedCustomer['indebtedness'] = value!,
               ),
               TextFormField(
-                initialValue: _customerData['currentAccount'],
+                initialValue: _editedCustomer['currentAccount'],
                 decoration: const InputDecoration(labelText: 'Current Account'),
-                onSaved: (value) => _customerData['currentAccount'] = value!,
+                onSaved: (value) => _editedCustomer['currentAccount'] = value!,
               ),
               TextFormField(
-                initialValue: _customerData['notes'],
+                initialValue: _editedCustomer['notes'],
                 decoration: const InputDecoration(labelText: 'Notes'),
-                onSaved: (value) => _customerData['notes'] = value!,
+                onSaved: (value) => _editedCustomer['notes'] = value!,
               ),
             ],
           ),
@@ -67,7 +153,7 @@ class _EditCustomerDialogState extends State<EditCustomerDialog> {
           onPressed: () {
             if (_formKey.currentState!.validate()) {
               _formKey.currentState!.save();
-              Navigator.of(context).pop(_customerData);
+              Navigator.of(context).pop(_editedCustomer);
             }
           },
           child: const Text('Save'),

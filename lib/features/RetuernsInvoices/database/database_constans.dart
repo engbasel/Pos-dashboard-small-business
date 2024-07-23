53867880adbd719c10d71aa65635307d447d7f83
Basel Embaby <engbasel2025@gmail.com>
2024-07-23 14:03:07 +0300

    refactor database helper

--- lib/features/RetuernsInvoices/database/database_constans.dart
+++ lib/features/RetuernsInvoices/database/database_constans.dart
@@ -1,6 +1,10 @@
 class RetuernInvocmentDatabaseConstants {
+  // =====================  Genranl Section in any database SQL helper ======================
   static const String returnInvoicesTable = 'return_invoices';
-
+  static const String databaseFileName = 'RetuernsInvoicesDatabase.db';
+  static const int versionDatabase = 4;
+// ======================  Genranl Section in any database SQL helper ======================
+// ======================  Columns Name of SQL helper ======================================
   static const String columnId = 'id';
   static const String columnOrderId = 'orderId';
   static const String columnReturnDate = 'returnDate';
@@ -8,4 +12,5 @@ class RetuernInvocmentDatabaseConstants {
   static const String columnReason = 'reason';
   static const String columnAmount = 'amount';
   static const String columnTotalBackMoney = 'totalbackmony';
+// ======================  Columns Name of SQL helper =======================================
 }

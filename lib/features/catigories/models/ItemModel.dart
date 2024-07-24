d6456f05014ef2a62cbeb0077284299ffb6bdcd6
Basel Embaby <engbasel2025@gmail.com>
2024-07-25 01:14:42 +0300

    "Removed implementation of CategoryScreen, ItemScreen, and CustomersView, leaving only import statements."

--- lib/features/catigories/models/ItemModel.dart
+++ lib/features/catigories/models/ItemModel.dart
@@ -1,34 +1,3 @@
-// class ItemModel {
-//   final int? id;
-//   final int categoryId;
-//   final String name;
-//   final String description; // Optional field for more details
-
-//   ItemModel(
-//       {this.id,
-//       required this.categoryId,
-//       required this.name,
-//       this.description = ''});
-
-//   Map<String, dynamic> toMap() {
-//     return {
-//       'id': id,
-//       'categoryId': categoryId,
-//       'name': name,
-//       'description': description,
-//     };
-//   }
-
-//   factory ItemModel.fromMap(Map<String, dynamic> map) {
-//     return ItemModel(
-//       id: map['id'],
-//       categoryId: map['categoryId'],
-//       name: map['name'],
-//       description: map['description'],
-//     );
-//   }
-// }
-
 class ItemModel {
   final int? id;
   final int? categoryId;

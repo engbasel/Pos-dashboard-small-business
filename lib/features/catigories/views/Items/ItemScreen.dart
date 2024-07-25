c93489d45e0f0125973f983c5989900685f550f3
Basel Embaby <engbasel2025@gmail.com>
2024-07-25 18:33:57 +0300

    "Removed image picker and file selector imports, removed showAddItemDialog and its associated widgets, removed buildListView and buildGridView methods, and updated FloatingActionButton onPressed function."

--- lib/features/catigories/views/Items/ItemScreen.dart
+++ lib/features/catigories/views/Items/ItemScreen.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
-
-import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:pos_dashboard_v1/features/catigories/database/item_database_helper.dart';
 import 'package:pos_dashboard_v1/features/catigories/models/ItemModel.dart';
-import 'ItemDetailsScreen.dart'; // Import the updated ItemDetailsScreen
+import 'package:pos_dashboard_v1/features/catigories/views/Items/item_dialog.dart';
+import 'package:pos_dashboard_v1/features/catigories/views/Items/item_grid_view.dart';
+import 'package:pos_dashboard_v1/features/catigories/views/Items/item_list_view.dart';
 import '../../../../l10n/app_localizations.dart';
 
 class ItemScreen extends StatefulWidget {
@@ -21,14 +19,11 @@ class _ItemScreenState extends State<ItemScreen> {
   List<ItemModel> items = [];
   List<ItemModel> filteredItems = [];
   TextEditingController searchController = TextEditingController();
-  bool isGridView = false; // Track the current view mode
-  late TextEditingController imageController;
+  bool isGridView = false;
 
   @override
   void initState() {
     super.initState();
-    imageController = TextEditingController();
-
     loadItems();
     searchController.addListener(filterItems);
   }
@@ -49,297 +44,6 @@ class _ItemScreenState extends State<ItemScreen> {
             item.description.toLowerCase().contains(query);
       }).toList();
     });
-  }
-
-  Future<String?> pickImage() async {
-    String? path;
-
-    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-      // Use file_selector for desktop
-      final XFile? file = await openFile(
-        acceptedTypeGroups: [
-          const XTypeGroup(label: 'Images', extensions: ['jpg', 'png', 'gif'])
-        ],
-      );
-      path = file?.path;
-    } else {
-      // Use image_picker for mobile
-      final picker = ImagePicker();
-      final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-      path = pickedFile?.path;
-    }
-
-    return path;
-  }
-
-  Future<void> showAddItemDialog(BuildContext context, int categoryId) async {
-    final formKey = GlobalKey<FormState>();
-    TextEditingController nameController = TextEditingController();
-    TextEditingController descriptionController = TextEditingController();
-    TextEditingController skuController = TextEditingController();
-    TextEditingController barcodeController = TextEditingController();
-    TextEditingController purchasePriceController = TextEditingController();
-    TextEditingController salePriceController = TextEditingController();
-    TextEditingController wholesalePriceController = TextEditingController();
-    TextEditingController taxRateController = TextEditingController();
-    TextEditingController quantityController = TextEditingController();
-    TextEditingController alertQuantityController = TextEditingController();
-    TextEditingController imageController = TextEditingController();
-    TextEditingController brandController = TextEditingController();
-    TextEditingController sizeController = TextEditingController();
-    TextEditingController weightController = TextEditingController();
-    TextEditingController colorController = TextEditingController();
-    TextEditingController materialController = TextEditingController();
-    TextEditingController warrantyController = TextEditingController();
-    TextEditingController supplierIdController = TextEditingController();
-    String? itemStatus;
-
-    await showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(AppLocalizations.of(context).translate('add_item')),
-          content: SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  // Button to pick an image
-                  TextButton(
-                    onPressed: () async {
-                      final path = await pickImage();
-                      if (path != null) {
-                        setState(() {
-                          imageController.text = path;
-                        });
-                      }
-                    },
-                    child: Text(
-                        AppLocalizations.of(context).translate('select_image')),
-                  ),
-                  // Display selected image (optional)
-                  imageController.text.isNotEmpty
-                      ? Image.file(File(imageController.text), height: 100)
-                      : const Icon(Icons.image, size: 100),
-
-                  TextFormField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)
-                            .translate('item_name')),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return AppLocalizations.of(context)
-                            .translate('name_required');
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: descriptionController,
-                    decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)
-                            .translate('description')),
-                  ),
-                  TextFormField(
-                    controller: skuController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('sku')),
-                  ),
-                  TextFormField(
-                    controller: barcodeController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('barcode')),
-                  ),
-                  TextFormField(
-                    controller: purchasePriceController,
-                    decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)
-                            .translate('purchase_price')),
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return AppLocalizations.of(context)
-                            .translate('purchase_price_required');
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: salePriceController,
-                    decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)
-                            .translate('sale_price')),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    controller: wholesalePriceController,
-                    decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)
-                            .translate('wholesale_price')),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    controller: taxRateController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('tax_rate')),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    controller: quantityController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('quantity')),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    controller: alertQuantityController,
-                    decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)
-                            .translate('alert_quantity')),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    controller: imageController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('image')),
-                  ),
-                  TextFormField(
-                    controller: brandController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('brand')),
-                  ),
-                  TextFormField(
-                    controller: sizeController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('size')),
-                  ),
-                  TextFormField(
-                    controller: weightController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('weight')),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    controller: colorController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('color')),
-                  ),
-                  TextFormField(
-                    controller: materialController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('material')),
-                  ),
-                  TextFormField(
-                    controller: warrantyController,
-                    decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context).translate('warranty')),
-                  ),
-                  TextFormField(
-                    controller: supplierIdController,
-                    decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)
-                            .translate('supplier_id')),
-                    keyboardType: TextInputType.number,
-                  ),
-                  DropdownButtonFormField<String>(
-                    value: itemStatus,
-                    decoration: InputDecoration(
-                        labelText: AppLocalizations.of(context)
-                            .translate('item_status')),
-                    items: ['active', 'inactive', 'discontinued']
-                        .map((status) => DropdownMenuItem(
-                              value: status,
-                              child: Text(AppLocalizations.of(context)
-                                  .translate(status)),
-                            ))
-                        .toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        itemStatus = value;
-                      });
-                    },
-                    validator: (value) {
-                      if (value == null) {
-                        return AppLocalizations.of(context)
-                            .translate('status_required');
-                      }
-                      return null;
-                    },
-                  ),
-// ===========
-                ],
-              ),
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text(AppLocalizations.of(context).translate('cancel')),
-            ),
-            TextButton(
-              onPressed: () async {
-                if (formKey.currentState?.validate() ?? false) {
-                  await ItemDatabaseHelper.instance.insertItem(
-                    ItemModel(
-                      id: null,
-                      categoryId: widget.categoryId,
-                      name: nameController.text,
-                      description: descriptionController.text,
-                      sku: skuController.text,
-                      barcode: barcodeController.text,
-                      purchasePrice:
-                          double.tryParse(purchasePriceController.text) ?? 0.0,
-                      salePrice:
-                          double.tryParse(salePriceController.text) ?? 0.0,
-                      wholesalePrice:
-                          double.tryParse(wholesalePriceController.text) ?? 0.0,
-                      taxRate: double.tryParse(taxRateController.text) ?? 0.0,
-                      quantity: int.tryParse(quantityController.text) ?? 0,
-                      alertQuantity:
-                          int.tryParse(alertQuantityController.text) ?? 0,
-                      image: imageController.text,
-                      brand: brandController.text,
-                      size: sizeController.text,
-                      weight: double.tryParse(weightController.text) ?? 0.0,
-                      color: colorController.text,
-                      material: materialController.text,
-                      warranty: warrantyController.text,
-                      supplierId: int.tryParse(supplierIdController.text) ?? 0,
-                      itemStatus: itemStatus ?? 'active',
-                    ),
-                  );
-                  loadItems();
-                  Navigator.of(context).pop();
-                }
-              },
-              child: Text(AppLocalizations.of(context).translate('add')),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    searchController.dispose();
-    imageController.dispose();
-
-    super.dispose();
   }
 
   Future<void> updateItem(ItemModel updatedItem) async {
@@ -388,101 +92,18 @@ class _ItemScreenState extends State<ItemScreen> {
                         .translate('item_not_available')),
                   )
                 : isGridView
-                    ? buildGridView()
-                    : buildListView(),
+                    ? buildGridView(context, filteredItems, updateItem)
+                    : buildListView(
+                        context, filteredItems, updateItem, loadItems),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showAddItemDialog(context, widget.categoryId);
+          showAddItemDialog(context, widget.categoryId, loadItems);
         },
         child: const Icon(Icons.add),
       ),
-    );
-  }
-
-  Widget buildListView() {
-    return ListView.builder(
-      itemCount: filteredItems.length,
-      itemBuilder: (context, index) {
-        final item = filteredItems[index];
-        return ListTile(
-          title: Text(item.name),
-          subtitle: Text(item.description),
-          trailing: IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () async {
-              await ItemDatabaseHelper.instance.deleteItem(item.id!);
-              loadItems();
-            },
-          ),
-          onTap: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => ItemDetailsScreen(item: item),
-              ),
-            );
-            if (result != null && result is ItemModel) {
-              updateItem(result);
-            }
-          },
-        );
-      },
-    );
-  }
-
-  Widget buildGridView() {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 5,
-        childAspectRatio: 3 / 2,
-      ),
-      itemCount: filteredItems.length,
-      itemBuilder: (context, index) {
-        final item = filteredItems[index];
-        return GestureDetector(
-          onTap: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => ItemDetailsScreen(item: item),
-              ),
-            );
-            if (result != null && result is ItemModel) {
-              updateItem(result);
-            }
-          },
-          child: Container(
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 4,
-                  offset: Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: item.image != null
-                        ? Image.file(File(item.image!), fit: BoxFit.cover)
-                        : const Icon(Icons.image, size: 50),
-                  ),
-                ),
-                Text(item.name, overflow: TextOverflow.ellipsis),
-                Text(item.description, overflow: TextOverflow.ellipsis),
-              ],
-            ),
-          ),
-        );
-      },
     );
   }
 }
---
+++ lib/features/catigories/views/Items/item_grid_view.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:pos_dashboard_v1/features/catigories/models/ItemModel.dart';
+import 'ItemDetailsScreen.dart';
+
+Widget buildGridView(
+    BuildContext context, List<ItemModel> filteredItems, Function updateItem) {
+  return GridView.builder(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 3, // 3 cards per row
+      childAspectRatio: 3 / 2, // Adjust aspect ratio as needed
+    ),
+    itemCount: filteredItems.length,
+    itemBuilder: (context, index) {
+      final item = filteredItems[index];
+      return Card(
+        margin: const EdgeInsets.all(8.0),
+        child: InkWell(
+          onTap: () async {
+            // Navigate to ItemDetailsScreen with the selected item
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ItemDetailsScreen(
+                  item: item,
+                ),
+              ),
+            );
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: item.image != null && item.image!.isNotEmpty
+                    ? Image.file(
+                        File(item.image!),
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      )
+                    : const Icon(Icons.image, size: 100),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  item.name,
+                  style: Theme.of(context).textTheme.titleLarge,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  '\$${item.salePrice?.toStringAsFixed(2)}',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}

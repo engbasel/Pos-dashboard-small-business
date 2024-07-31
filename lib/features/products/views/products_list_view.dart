fa73cf6de41f128f7c9d10737698a041eab73832
Abdelrahman Hussein <elhussieny100@gmail.com>
2024-07-31 19:55:52 +0300

    image picker for the product

--- lib/features/products/views/products_list_view.dart
+++ lib/features/products/views/products_list_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pos_dashboard_v1/core/widgets/custom_app_bar.dart';
+import 'package:pos_dashboard_v1/core/widgets/custom_button.dart';
 import 'package:pos_dashboard_v1/core/widgets/custom_small_button.dart';
+import 'package:pos_dashboard_v1/features/categories/views/Items/image_picker_helper.dart';
 import 'package:pos_dashboard_v1/features/products/views/products_item_details_view.dart';
 import '../../../core/db/new_products_store_database_helper.dart';
 import '../../categories/database/category_database_helper.dart';
@@ -26,6 +28,7 @@ class _ProductsListViewState extends State<ProductsListView> {
   List<ItemModel> items = [];
   List<CategoryModel> categories = [];
   String? selectedCategory;
+  String? path;
 
   final Map<String, TextEditingController> controllers = {
     'name': TextEditingController(),
@@ -92,6 +95,7 @@ class _ProductsListViewState extends State<ProductsListView> {
       itemStatus: 'active',
       dateAdded: DateTime.now(),
       dateModified: DateTime.now(),
+      image: path,
     );
 
     await itemDatabaseHelper.insertItem(newItem);
@@ -114,70 +118,98 @@ class _ProductsListViewState extends State<ProductsListView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            CustomAppBar(
-              title: AppLocalizations.of(context).translate('Products'),
-              actions: [
-                CustomSmallButton(
-                  icon: Icons.add,
-                  text: 'Add A Product',
-                  onTap: () => showCustomDialog(context),
-                ),
-                CustomSmallButton(
-                  icon: Icons.table_chart,
-                  text: 'View Table',
-                  onTap: navigateToItemsTable,
-                ),
-              ],
+    return Column(
+      children: [
+        CustomAppBar(
+          title: AppLocalizations.of(context).translate('Products'),
+          actions: [
+            CustomSmallButton(
+              icon: Icons.add,
+              text: 'Add A Product',
+              onTap: () => showCustomDialog(context),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  final item = items[index];
-                  return Card(
-                    elevation: 2,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      leading: item.image != null
-                          ? Image.file(
-                              File(item.image!),
-                              width: 50,
-                              height: 50,
-                              fit: BoxFit.cover,
-                            )
-                          : const Icon(Icons.image, size: 50),
-                      title: Text(item.name),
-                      subtitle: Text(item.description ?? ''),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete, color: Colors.red),
-                        onPressed: () => removeItem(item.id!),
-                      ),
-                      onTap: () {
-                        // Navigate to details page if needed
-                      },
-                    ),
-                  );
-                },
-              ),
+            const SizedBox(width: 8),
+            CustomSmallButton(
+              icon: Icons.table_chart,
+              text: 'View Table',
+              onTap: navigateToItemsTable,
             ),
           ],
         ),
-      ),
+        Expanded(
+          child: Container(
+            color: Colors.white,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ListTile(
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                    leading: item.image != null
+                        ? Image.file(
+                            File(item.image!),
+                            width: 50,
+                            height: 50,
+                            fit: BoxFit.cover,
+                          )
+                        : const Icon(Icons.image, size: 50),
+                    title: Text(item.name),
+                    subtitle: Text(item.description ?? ''),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => removeItem(item.id!),
+                    ),
+                    onTap: () {
+                      // Navigate to details page if needed
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 
   Widget buildInputSection() {
     return Column(
       children: [
+        GestureDetector(
+          onTap: () async {
+            final _path = await pickImage();
+            if (_path != null) {
+              path = _path;
+              setState(() {});
+            }
+          },
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: path != null
+                ? Image.file(
+                    File(path!),
+                    fit: BoxFit.cover,
+                  )
+                : Icon(
+                    Icons.add_a_photo,
+                    color: Colors.grey[800],
+                  ),
+          ),
+        ),
+        const SizedBox(height: 8),
         ...controllers.entries.map((entry) {
           final label = entry.key;
           return Padding(
@@ -216,15 +248,10 @@ class _ProductsListViewState extends State<ProductsListView> {
             ),
           ),
         ),
-        const SizedBox(height: 8),
-        ElevatedButton(
-          onPressed: addItem,
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ),
-          child: Text(AppLocalizations.of(context).translate('add')),
+        const SizedBox(height: 26),
+        CustomButton(
+          text: AppLocalizations.of(context).translate('add'),
+          onTap: addItem,
         ),
       ],
     );
@@ -236,8 +263,12 @@ class _ProductsListViewState extends State<ProductsListView> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text(AppLocalizations.of(context).translate('add_a_product')),
-          content: SingleChildScrollView(
-            child: buildInputSection(),
+          content: SizedBox(
+            width: MediaQuery.of(context).size.width * .5,
+            height: MediaQuery.of(context).size.height * .75,
+            child: SingleChildScrollView(
+              child: buildInputSection(),
+            ),
           ),
         );
       },

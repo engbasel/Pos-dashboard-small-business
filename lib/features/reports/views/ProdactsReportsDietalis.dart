cb5b9d1af8d45c25a2bee989ce94c77079ebb80b
Basel Embaby <engbasel2025@gmail.com>
2024-08-06 15:11:32 +0300

    "Added CustomAppBar with buttons and navigation to ProdactsReportsDietalis widget"

--- lib/features/reports/views/ProdactsReportsDietalis.dart
+++ lib/features/reports/views/ProdactsReportsDietalis.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:pos_dashboard_v1/core/widgets/custom_app_bar.dart';
+import 'package:pos_dashboard_v1/core/widgets/custom_small_button.dart';
+import 'package:pos_dashboard_v1/l10n/app_localizations.dart';
 
 class ProdactsReportsDietalis extends StatelessWidget {
   const ProdactsReportsDietalis({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          CustomAppBar(
+            title: AppLocalizations.of(context).translate('clientsReports'),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomSmallButton(
+                  icon: Icons.print,
+                  text: AppLocalizations.of(context).translate('printReport'),
+                  // onTap: printPDF,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomSmallButton(
+                  icon: Icons.picture_as_pdf,
+                  text: AppLocalizations.of(context).translate('ExportasPDF'),
+                  // onTap: exportPDF,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_forward,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

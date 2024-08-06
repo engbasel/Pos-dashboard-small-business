79798be6794a5dcd688bff090f0e457ba7f20701
Basel Embaby <engbasel2025@gmail.com>
2024-08-06 14:17:18 +0300

    "Deleted DatabaseHelper class and modified AllReportesViwer and AppLocalizations files"

---
+++ lib/features/reports/views/ProdactsReportsDietalis.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProdactsReportsDietalis extends StatelessWidget {
+  const ProdactsReportsDietalis({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}

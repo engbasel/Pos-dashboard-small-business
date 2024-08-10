48d972c4950fae8f554e0330e742e9dd4162125a
Basel Embaby <engbasel2025@gmail.com>
2024-08-10 15:03:14 +0300

    listvieew

--- lib/features/products/views/products_list_view.dart
+++ lib/features/products/views/products_list_view.dart
@@ -1,15 +1,74 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:pos_dashboard_v1/core/utils/manager/manager.dart';
+import 'package:pos_dashboard_v1/core/utils/Manager/manager.dart';
 import 'package:pos_dashboard_v1/core/widgets/custom_app_bar.dart';
 import 'package:pos_dashboard_v1/core/widgets/custom_small_button.dart';
 import 'package:pos_dashboard_v1/core/widgets/delete_conformation_dialog.dart';
+import 'package:pos_dashboard_v1/features/categories/database/category_database_helper.dart';
 import 'package:pos_dashboard_v1/features/categories/database/item_database_helper.dart';
+import 'package:pos_dashboard_v1/features/categories/models/category_model.dart';
+import 'package:pos_dashboard_v1/features/categories/models/item_model.dart';
+import 'package:pos_dashboard_v1/features/categories/views/Items/widgets/item_details_screen.dart';
 import 'package:pos_dashboard_v1/features/products/widgets/add_product_dialog.dart';
 import 'package:pos_dashboard_v1/features/products/widgets/product_list_item.dart';
-import '../../../l10n/app_localizations.dart';
-import '../../categories/database/category_database_helper.dart';
-import '../../categories/models/category_model.dart';
-import '../../categories/models/item_model.dart';
+import 'package:pos_dashboard_v1/l10n/app_localizations.dart';
+
+Widget buildGridView(
+    BuildContext context, List<ItemModel> filteredItems, Function updateItem) {
+  return GridView.builder(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 3, // 3 cards per row
+      childAspectRatio: 3 / 2, // Adjust aspect ratio as needed
+      crossAxisSpacing: 8.0, // Space between cards
+      mainAxisSpacing: 8.0, // Space between cards
+    ),
+    itemCount: filteredItems.length,
+    itemBuilder: (context, index) {
+      final item = filteredItems[index];
+      return Card(
+        margin: const EdgeInsets.all(8.0),
+        child: InkWell(
+          onTap: () async {
+            // Navigate to ItemDetailsScreen with the selected item
+            showDialog(
+              context: context,
+              builder: (context) => ItemDetailsDialog(item: item),
+            );
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: item.image != null && item.image!.isNotEmpty
+                    ? Image.file(
+                        File(item.image!),
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      )
+                    : const Icon(Icons.image, size: 100),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  item.name,
+                  style: Theme.of(context).textTheme.titleLarge,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  '\$${item.unitPrice?.toStringAsFixed(2)}',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
 
 class ProductsListView extends StatefulWidget {
   const ProductsListView({super.key});
@@ -23,6 +82,7 @@ class _ProductsListViewState extends State<ProductsListView> {
   final CategoryDatabaseHelper categoryDatabaseHelper =
       CategoryDatabaseHelper.instance;
   TextEditingController searchController = TextEditingController();
+  bool isGridView = false; // Track the current view mode
 
   List<ItemModel> items = [];
   List<ItemModel> filteredItems = [];
@@ -83,8 +143,16 @@ class _ProductsListViewState extends State<ProductsListView> {
           actions: [
             CustomSmallButton(
               icon: Icons.add,
-              text: AppLocalizations.of(context).translate('AddAProduc'),
+              text: AppLocalizations.of(context).translate('AddAProduct'),
               onTap: () => showAddProductDialog(context, categories, loadItems),
+            ),
+            IconButton(
+              icon: Icon(isGridView ? Icons.list : Icons.grid_view),
+              onPressed: () {
+                setState(() {
+                  isGridView = !isGridView;
+                });
+              },
             ),
           ],
         ),
@@ -113,6 +181,12 @@ class _ProductsListViewState extends State<ProductsListView> {
                     color: ColorsManager.kPrimaryColor,
                   ),
                 ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    // You can add search functionality here if needed
+                  },
+                ),
               ),
             ),
           ),
@@ -122,10 +196,8 @@ class _ProductsListViewState extends State<ProductsListView> {
             color: Colors.white,
             margin: const EdgeInsets.all(16),
             padding: const EdgeInsets.all(16),
-            child: filteredItems.isEmpty
-                ? const Center(
-                    child: Text(''),
-                  )
+            child: isGridView
+                ? buildGridView(context, filteredItems, loadItems)
                 : ListView.builder(
                     itemCount: filteredItems.length,
                     itemBuilder: (context, index) {

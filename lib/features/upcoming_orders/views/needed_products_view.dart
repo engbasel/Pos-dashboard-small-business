bb76bf66c0d3340795ed3e2d3ada3bfb9eec9323
Basel Embaby <engbasel2025@gmail.com>
2024-08-10 12:10:21 +0300

    feat: Refactor order item UI component
    Refactored the order item UI component to improve readability and maintainability. Changes include:
    
    * Extracted UI components into separate widgets for better organization
    * Improved spacing and padding for a more consistent layout
    * Added `BorderRadius` to `InkWell` for a smoother touch experience
    * Used `const` where possible to reduce unnecessary rebuilds
    * Reformatted code to follow standard Dart coding conventions

--- lib/features/upcoming_orders/views/needed_products_view.dart
+++ lib/features/upcoming_orders/views/needed_products_view.dart
@@ -10,13 +10,6 @@ import 'package:pos_dashboard_v1/features/upcoming_orders/widgets/needed_product
 import '../../../l10n/app_localizations.dart';
 import '../model/incoming_order_model.dart';
 
-// class NeededProductsView extends StatefulWidget {
-//   const NeededProductsView({super.key});
-
-//   @override
-//   State<NeededProductsView> createState() => _NeededProductsViewState();
-// }
-
 class NeededProductsView extends StatefulWidget {
   const NeededProductsView({super.key});
 
@@ -25,13 +18,21 @@ class NeededProductsView extends StatefulWidget {
 }
 
 class _NeededProductsViewState extends State<NeededProductsView> {
-  late Future<List<IncomingOrderModel>> _incomingOrdersFuture;
-  String? _selectedDate; // Store the selected date
+  late Future<List<IncomingOrderModel>> incomingOrdersFuture;
+  String? selectedDate;
+  final TextEditingController _searchController = TextEditingController();
+  String searchQuery = '';
 
   @override
   void initState() {
     super.initState();
-    _incomingOrdersFuture = DatabaseIncomingOrdersManager().getIncomingOrders();
+    incomingOrdersFuture = DatabaseIncomingOrdersManager().getIncomingOrders();
+
+    _searchController.addListener(() {
+      setState(() {
+        searchQuery = _searchController.text;
+      });
+    });
   }
 
   void showCustomDialog(BuildContext context) async {
@@ -47,7 +48,7 @@ class _NeededProductsViewState extends State<NeededProductsView> {
     );
 
     if (result == true) {
-      _incomingOrdersFuture =
+      incomingOrdersFuture =
           DatabaseIncomingOrdersManager().getIncomingOrders();
       setState(() {});
     }
@@ -85,7 +86,7 @@ class _NeededProductsViewState extends State<NeededProductsView> {
     );
 
     if (result == true) {
-      _incomingOrdersFuture =
+      incomingOrdersFuture =
           DatabaseIncomingOrdersManager().getIncomingOrders();
       setState(() {});
     }
@@ -102,14 +103,13 @@ class _NeededProductsViewState extends State<NeededProductsView> {
     if (confirmed == true) {
       await DatabaseIncomingOrdersManager().deleteIncomingOrder(itemId);
       setState(() {
-        _incomingOrdersFuture =
+        incomingOrdersFuture =
             DatabaseIncomingOrdersManager().getIncomingOrders();
       });
     }
   }
 
-  // Function to show date picker and filter orders by date
-  void _filterByDate() async {
+  void filterByDate() async {
     DateTime? pickedDate = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
@@ -119,50 +119,117 @@ class _NeededProductsViewState extends State<NeededProductsView> {
 
     if (pickedDate != null) {
       setState(() {
-        _selectedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
-        _incomingOrdersFuture =
-            DatabaseIncomingOrdersManager().getOrdersByDay(_selectedDate!);
+        selectedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+        incomingOrdersFuture =
+            DatabaseIncomingOrdersManager().getOrdersByDay(selectedDate!);
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomAppBar(
-          title: AppLocalizations.of(context).translate('NeededProducts'),
-          actions: [
-            CustomSmallButton(
-              icon: Icons.add,
-              text: AppLocalizations.of(context).translate('AddAProduc'),
-              onTap: () async {
-                showCustomDialog(context);
-              },
+    return Scaffold(
+      body: Column(
+        children: [
+          CustomAppBar(
+            title: AppLocalizations.of(context).translate('NeededProducts'),
+            actions: [
+              CustomSmallButton(
+                icon: Icons.add,
+                text: AppLocalizations.of(context).translate('AddAProduct'),
+                onTap: () async {
+                  showCustomDialog(context);
+                },
+              ),
+              // IconButton(
+              //   icon: const Icon(Icons.filter_list, color: Colors.white),
+              //   onPressed: filterByDate,
+              // ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Material(
+                    color: Colors.white,
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        hintText:
+                            AppLocalizations.of(context).translate('search'),
+                        prefixIcon: const Icon(Icons.search),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: ColorsManager.kPrimaryColor,
+                          ),
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: ColorsManager.kPrimaryColor,
+                          ),
+                        ),
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: ColorsManager.kPrimaryColor,
+                          ),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          searchQuery = value;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Container(
+                  decoration: BoxDecoration(
+                    color: ColorsManager.kPrimaryColor,
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.filter_list,
+                      color: Colors.white,
+                    ),
+                    onPressed: filterByDate,
+                  ),
+                ),
+              ],
             ),
-            IconButton(
-              icon: const Icon(Icons.calendar_today),
-              onPressed: _filterByDate, // Trigger the date picker
-            ),
-          ],
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.white,
-            margin: const EdgeInsets.all(16),
-            padding: const EdgeInsets.all(16),
+          ),
+          const SizedBox(height: 8),
+          Expanded(
             child: FutureBuilder<List<IncomingOrderModel>>(
-              future: _incomingOrdersFuture,
+              future: incomingOrdersFuture,
               builder: (context, snapshot) {
                 if (snapshot.hasError) {
                   return const Center(child: Text('Error occurred'));
                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
                   return const Center(child: Text('No orders found'));
                 } else {
+                  List<IncomingOrderModel> filteredOrders = snapshot.data!
+                      .where((order) =>
+                          order.orderId
+                              .toLowerCase()
+                              .contains(searchQuery.toLowerCase()) ||
+                          order.supplierName
+                              .toLowerCase()
+                              .contains(searchQuery.toLowerCase()))
+                      .toList();
+
+                  if (filteredOrders.isEmpty) {
+                    return const Center(child: Text('No orders match search'));
+                  }
+
                   return ListView.builder(
-                    itemCount: snapshot.data!.length,
+                    itemCount: filteredOrders.length,
                     itemBuilder: (context, index) {
-                      final order = snapshot.data![index];
+                      final order = filteredOrders[index];
                       return Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: InkWell(
@@ -243,210 +310,8 @@ class _NeededProductsViewState extends State<NeededProductsView> {
               },
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
-
-// class _NeededProductsViewState extends State<NeededProductsView> {
-//   late Future<List<IncomingOrderModel>> _incomingOrdersFuture;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _incomingOrdersFuture = DatabaseIncomingOrdersManager().getIncomingOrders();
-//   }
-
-//   void showCustomDialog(BuildContext context) async {
-//     final result = await showDialog<bool>(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           backgroundColor: Colors.white,
-//           title: Text(AppLocalizations.of(context).translate('Add a product')),
-//           content: const NeededProductsForm(),
-//         );
-//       },
-//     );
-
-//     if (result == true) {
-//       _incomingOrdersFuture =
-//           DatabaseIncomingOrdersManager().getIncomingOrders();
-//       setState(() {});
-//     }
-//   }
-
-//   void showProductsDetails(IncomingOrderModel product) {
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return Dialog(
-//           backgroundColor: Colors.white,
-//           shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.circular(16),
-//           ),
-//           child: NeededProductsDetails(
-//             product: product,
-//           ),
-//         );
-//       },
-//     );
-//   }
-
-//   void showEditDialog(IncomingOrderModel order) async {
-//     final result = await showDialog<bool>(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           backgroundColor: Colors.white,
-//           title: Text(AppLocalizations.of(context).translate('Edit a product')),
-//           content: NeededProductsForm(
-//             order: order,
-//           ),
-//         );
-//       },
-//     );
-
-//     if (result == true) {
-//       _incomingOrdersFuture =
-//           DatabaseIncomingOrdersManager().getIncomingOrders();
-//       setState(() {});
-//     }
-//   }
-
-//   Future<void> deleteItem(String itemId) async {
-//     final confirmed = await showDialog<bool>(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return const DeleteConformationDialog();
-//       },
-//     );
-
-//     if (confirmed == true) {
-//       await DatabaseIncomingOrdersManager().deleteIncomingOrder(itemId);
-//       setState(() {
-//         _incomingOrdersFuture =
-//             DatabaseIncomingOrdersManager().getIncomingOrders();
-//       });
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         CustomAppBar(
-//           title: AppLocalizations.of(context).translate('NeededProducts'),
-//           actions: [
-//             CustomSmallButton(
-//               icon: Icons.add,
-//               text: AppLocalizations.of(context).translate('AddAProduc'),
-//               onTap: () async {
-//                 showCustomDialog(context);
-//               },
-//             ),
-//           ],
-//         ),
-//         Expanded(
-//           child: Container(
-//             color: Colors.white,
-//             margin: const EdgeInsets.all(16),
-//             padding: const EdgeInsets.all(16),
-//             child: FutureBuilder<List<IncomingOrderModel>>(
-//               future: _incomingOrdersFuture,
-//               builder: (context, snapshot) {
-//                 if (snapshot.hasError) {
-//                   return const Center(child: Text(''));
-//                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-//                   return const Center(child: Text(''));
-//                 } else {
-//                   return ListView.builder(
-//                     itemCount: snapshot.data!.length,
-//                     itemBuilder: (context, index) {
-//                       final order = snapshot.data![index];
-//                       return Padding(
-//                         padding: const EdgeInsets.all(8.0),
-//                         child: InkWell(
-//                           borderRadius: BorderRadius.circular(8.0),
-//                           onTap: () {
-//                             showProductsDetails(order);
-//                           },
-//                           child: Container(
-//                             padding: const EdgeInsets.symmetric(
-//                               horizontal: 16,
-//                               vertical: 8,
-//                             ),
-//                             decoration: BoxDecoration(
-//                               color: ColorsManager.backgroundColor,
-//                               borderRadius: BorderRadius.circular(8.0),
-//                             ),
-//                             child: Row(
-//                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                               children: [
-//                                 Column(
-//                                   crossAxisAlignment: CrossAxisAlignment.start,
-//                                   children: [
-//                                     Text(
-//                                       'Order ID : ${order.orderId}',
-//                                       style: const TextStyle(
-//                                         fontWeight: FontWeight.w400,
-//                                         fontSize: 16.0,
-//                                       ),
-//                                     ),
-//                                     const SizedBox(height: 3),
-//                                     Text(
-//                                       'Supplier : ${order.supplierName}',
-//                                       style: const TextStyle(
-//                                         fontWeight: FontWeight.w400,
-//                                         fontSize: 16.0,
-//                                       ),
-//                                     ),
-//                                     const SizedBox(height: 3),
-//                                     Text(
-//                                       'Total Amount : \$${order.totalAmount.toStringAsFixed(2)}',
-//                                       style: const TextStyle(
-//                                         fontWeight: FontWeight.w400,
-//                                         fontSize: 16.0,
-//                                       ),
-//                                     ),
-//                                   ],
-//                                 ),
-//                                 Row(
-//                                   children: [
-//                                     IconButton(
-//                                       onPressed: () {
-//                                         showEditDialog(order);
-//                                       },
-//                                       icon: const Icon(
-//                                         Icons.edit,
-//                                         color: ColorsManager.kPrimaryColor,
-//                                       ),
-//                                     ),
-//                                     IconButton(
-//                                       onPressed: () {
-//                                         deleteItem(order.id);
-//                                       },
-//                                       icon: const Icon(
-//                                         Icons.delete,
-//                                         color: Colors.red,
-//                                       ),
-//                                     ),
-//                                   ],
-//                                 ),
-//                               ],
-//                             ),
-//                           ),
-//                         ),
-//                       );
-//                     },
-//                   );
-//                 }
-//               },
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
